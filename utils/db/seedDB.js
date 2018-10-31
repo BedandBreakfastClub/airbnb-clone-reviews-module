817875ece6772781dee149f5664bae97e3f3847c
Gora Kong <gora.kong93@gmail.com>
2018-10-31 11:05:28 -0700

    edited seeding script

--- utils/db/seedDB.js
+++ utils/db/seedDB.js
@@ -6,14 +6,14 @@ const listings = [];
 
 function generateReview() {
   let count = 0;
-  while (count < 4) {
+  while (count < 100) {
     listings.push(faker.random.uuid());
     count += 1;
   }
 
   const reviewObj = {
     id: faker.random.uuid(),
-    listing_id: listings[faker.random.number(3)],
+    listing_id: listings[faker.random.number(99)],
     author: faker.name.findName(),
     user_id: faker.random.uuid(),
     avatar_url: faker.image.avatar(),
